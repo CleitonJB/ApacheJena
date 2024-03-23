169786fec93480078f19ecec523465a33828a673
Cleiton Braga <cleitonbraga56@gmail.com>
2024-03-23 15:38:53 -0300

    Create 'Hello World' project with Apache Jena

--- sistemaSuporteDecisaoMaven/src/main/java/iff/sistemaSuporteDecisaoMaven/App.java
+++ sistemaSuporteDecisaoMaven/src/main/java/iff/sistemaSuporteDecisaoMaven/App.java
@@ -1,13 +1,29 @@
 package iff.sistemaSuporteDecisaoMaven;
 
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	static private String URI = "http://example.org/test/";
+
+    public static void main(String args[])
     {
-        System.out.println( "Hello World!" );
+    	Model model = ModelFactory.createDefaultModel();
+    	
+    	Resource subject = model.createResource(URI + "message");
+    	
+    	Property property = model.createProperty(URI + "says");
+    	
+    	subject.addProperty(property, "Hello World!", XSDDatatype.XSDstring);
+    	
+        model.write(System.out);
     }
 }
